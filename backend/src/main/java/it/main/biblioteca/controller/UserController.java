f0a5074dbc03554bee5bd1d5ca6493f69d6dde9a
mauro-tram <mauro.tramentozzi99@gmail.com>
2025-02-17 22:55:10 +0100

    Created last CRUD methods for User entity.

--- backend/src/main/java/it/main/biblioteca/controller/UserController.java
+++ backend/src/main/java/it/main/biblioteca/controller/UserController.java
@@ -3,6 +3,7 @@ package it.main.biblioteca.controller;
 import it.main.biblioteca.model.User;
 import it.main.biblioteca.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,9 +20,30 @@ public class UserController {
         return userService.getAllUsers();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     public User addUser(@RequestBody User user) {
-        System.out.println(user.toString());
         return userService.addUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @PathVariable User updatedUser) {
+        return userService.updateUser(id, updatedUser)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        boolean deleted = userService.deleteUser(id);
+        return deleted ?
+                ResponseEntity.noContent().build() :
+                ResponseEntity.notFound().build();
     }
 }
--- backend/src/main/java/it/main/biblioteca/service/UserService.java
+++ backend/src/main/java/it/main/biblioteca/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,7 +18,26 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
     public User addUser(User user) {
         return userRepository.save(user);
+    }
+
+    public Optional<User> updateUser(Long id, User updatedUser) {
+        return userRepository.findById(id).map(existingUser -> {
+            existingUser.setName(updatedUser.getName());
+            return userRepository.save(existingUser);
+        });
+    }
+
+    public boolean deleteUser(Long id) {
+        if(userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
