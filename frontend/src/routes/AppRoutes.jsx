ac18c744ec081b88ffba15a3e380b49b0f53097f
mauro-tram <mauro.tramentozzi99@gmail.com>
2025-03-06 22:49:19 +0100

    Single User route in AppRoutes.

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from "../pages/Homepage.jsx";
 import SingleBook from '../pages/SingleBook.jsx';
+import SingleUser from '../pages/SingleUser.jsx';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/books/1" element={<SingleBook />} />
+            <Route path="/users/1" element={<SingleUser />} />
         </Routes>
     );
 };
