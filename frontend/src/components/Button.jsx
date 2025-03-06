d81367473534cb85326851770990e4bed7706613
mauro-tram <mauro.tramentozzi99@gmail.com>
2025-03-06 22:47:40 +0100

    Updated Button component (classes).

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Button({ title = "Click!", primary = true, isClickable = true }) {
-    const classBase = "w-full px-10 py-4 mt-8 text-3xl disabled:opacity-75";
-    const classType = primary ? "bg-actions text-secondary" : "bg-tertiary text-black";
-
+    const classBase = "border-2 w-full px-10 py-4 mt-8 text-3xl cursor-pointer shadow transition-all disabled:cursor-not-allowed disabled:opacity-75 disabled:shadow-none focus:border-[#191919] hover:shadow-none";
+    const classType = primary ?
+        "bg-actions border-actions text-secondary" :
+        "bg-tertiary border-tertiary text-black"
+        ;
 
     return (
         <button
