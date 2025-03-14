c2f1b093e93f46775a27571da5fcff81228ecfde
mauro-tram <mauro.tramentozzi99@gmail.com>
2025-03-14 16:24:22 +0100

    Updated Header and Footer.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div className="fixed bottom-0 flex justify-between w-full p-4 mt-auto text-secondary bg-primary footer">
-            <a href="http://localhost:3000">Library App</a>
+        <footer className="flex justify-between w-full p-4 mt-auto text-secondary bg-primary footer">
+            <Link to={"/"}>
+                <p>Library App</p>
+            </Link>
             <p className="text-center">&copy; 2025 Mauro Tramentozzi. Tutti i diritti riservati.</p>
-        </div>
+        </footer>
     );
 };
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,14 +2,10 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className="fixed top-0 flex justify-between w-full p-3 px-6 text-xl text-secondary bg-primary header">
+        <header className="flex justify-between w-full p-3 px-6 text-xl text-secondary bg-primary header">
             <h2>Benvenuto, Bibliotecario!</h2>
-            <span className="flex items-center gap-4">
-                <p>Utenti</p>
-                <p>Libri</p>
-            </span>
             <a href="https://www.linkedin.com/in/mauro-tramentozzi-developer">LinkedIn</a>
-        </div>
+        </header>
     );
 };
 
